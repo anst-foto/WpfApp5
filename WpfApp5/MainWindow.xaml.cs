762e25ce243badbdb11dd6e96dd338ba107e6499
Starinin Andrey <starinin-andrey@ya.ru>
2024-12-14 21:18:28 +0300

    Initial commit

---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WpfApp5;
+
+public partial class MainWindow : Window, INotifyPropertyChanged
+{
+    private long? _sum;
+    public long? Sum
+    {
+        get => _sum;
+        set => SetField(ref _sum, value);
+    }
+
+    private long? _factorial;
+    public long? Factorial
+    {
+        get => _factorial;
+        set => SetField(ref _factorial, value);
+    }
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private async void ButtonCalculate_OnClick(object sender, RoutedEventArgs e)
+    {
+        ButtonCalculate.IsEnabled = false;
+        
+        var number = Convert.ToInt32(ВводЧисла.Text);
+
+        var progressSum = new Progress<int>();
+        progressSum.ProgressChanged += (s, i) =>
+        {
+            ProgressSum.Value = i;
+        };
+        
+        var progressFactorial = new Progress<int>();
+        progressFactorial.ProgressChanged += (s, i) =>
+        {
+            ProgressFactorial.Value = i;
+        };
+
+        await Task.Run(async () =>
+        {
+            Sum = await Калькулятор.СуммаAsync(number, progressSum);
+        });
+        
+        await Task.Run(async () =>
+        {
+           Factorial = await Калькулятор.ФакториалAsync(number, progressFactorial);
+        });
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
---
+++ WpfApp5/Калькулятор.cs
@@ -0,0 +1,43 @@
+﻿namespace WpfApp5;
+
+public static class Калькулятор
+{
+    public static long Факториал(int число)
+    {
+        if (число == 0) return 1;
+        Task.Delay(1000).Wait();
+        return число * Факториал(число - 1);
+    }
+    public static async Task<long> ФакториалAsync(int number, IProgress<int>? progress = null)
+    {
+        return await Task.Run(async () =>
+        {
+            long factorial = 1;
+
+            for (int i = number; i > 1; i--)
+            {
+                progress?.Report(number - i);
+                factorial *= i;
+                await Task.Delay(1000);
+            }
+            
+            return factorial;
+        });
+    }
+
+    public static async Task<long> СуммаAsync(int число, IProgress<int>? progress = null)
+    {
+        return await Task.Run(async () =>
+        {
+            long sum = 0;
+            for (int i = число; i > 0; i--)
+            {
+                progress?.Report(число - i);
+                sum += i;
+                await Task.Delay(10);
+            }
+
+            return sum;
+        });
+    }
+}
